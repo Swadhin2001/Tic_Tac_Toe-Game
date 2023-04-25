673baaf7099e69706eac7a596c7d28b62f9152d6
Swadhin2001 <2025.it18.swadhinpaul@gmail.com>
2023-04-25 22:37:47 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/Footer';
 import Tic_Tac_Toe from './components/Tic_Tac_Toe';
 
 function App() {
   return (
    <>
-   <Tic_Tac_Toe></Tic_Tac_Toe>
+    <Tic_Tac_Toe></Tic_Tac_Toe>
+    <Footer></Footer>
    </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <div className="footer">
+        <p>Copyright © Swadhin Paul</p>
+    </div>
+    </>
+  )
+}
